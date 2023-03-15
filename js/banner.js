906d937e390d912161e972ffa29c94550f81ddf9
mi nombre <desarrollowebaps@gmail.com>
2023-03-14 22:51:55 -0400

    05_template_financial

--- js/banner.js
+++ js/banner.js
@@ -1,7 +1,7 @@
 let container = document.querySelector("#container")
 let cajas = document.querySelectorAll("#cajas")
-        let arrowLeft = document.querySelector(".arrowLeft")
-        let arrowRight = document.querySelector(".arrowRight")
+let arrowLeft = document.querySelector(".arrowLeft")
+let arrowRight = document.querySelector(".arrowRight")
 
 
         let ultimoEle = cajas[cajas.length - 1]
@@ -11,20 +11,20 @@ let cajas = document.querySelectorAll("#cajas")
         document.addEventListener("click", e =>{
         if(e.target.classList.contains("arrowRight")){
             let cajasB = document.querySelectorAll("#cajas")
-            let enFoco = cajasB[cajasB.length - 4 ]
+            let primerEle = cajasB[cajasB.length - 3 ]
             container.style.marginLeft = "-300%"
-            container.style.transition = "0.5s all"
+            container.style.transition = "1s linear"
             setTimeout(() =>{
                 container.style.transition = "none"
-                container.insertAdjacentElement("beforeend", enFoco)
-                container.style.marginLeft = "-200%"
+                container.insertAdjacentElement("beforeend", primerEle)
+                container.style.marginLeft = "-100%"
             },300)
             
         }else if(e.target.classList.contains("arrowLeft")){
             let elementos = document.querySelectorAll("#cajas")
             let ultimoBloque = elementos[elementos.length - 1]
             container.style.marginLeft = "0"
-            container.style.transition = "0.5s all"
+            container.style.transition = "1s all"
             setTimeout(()=>{
                 container.style.transition = "none"
                 container.insertAdjacentElement("afterbegin", ultimoBloque)
@@ -32,3 +32,4 @@ let cajas = document.querySelectorAll("#cajas")
             },300)  
           }
         })
+        
