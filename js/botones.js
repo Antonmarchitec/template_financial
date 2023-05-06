36b73a93acae3500a7104e3c281a6c7c8df1b23a
mi nombre <desarrollowebaps@gmail.com>
2023-05-05 21:27:10 -0400

    20_template_financial

--- js/botones.js
+++ js/botones.js
@@ -1,3 +1,7 @@
+
+
+// SCRIPT BOTON SUBIR
+
 let botonTop = document.querySelector("#botonTop")
 
 document.addEventListener("scroll", ()=>{
@@ -14,3 +18,20 @@ document.addEventListener("scroll", ()=>{
         botonTop.classList.remove("boton_top_JS")
     }
 })
+
+// SCRIPT BOTON SUBIR
+
+
+
+
+// SCRIPT BOTON HAMBURGUER
+let btnHamburguer = document.querySelector("#btnHamburguer")
+let contHeaderMenu__menu = document.getElementById("contHeaderMenu__menu")
+
+btnHamburguer.addEventListener("click", ()=>{
+    console.log("presionaste el botn ")
+    //contHeaderMenu__menu.classList.add("contHeaderMenu__menu__JS")
+
+})
+
+// SCRIPT BOTON HAMBURGUER
